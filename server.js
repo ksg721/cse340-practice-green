26b81e6c7b9f5b0e406e9949a7e538ca0239720c
ksg721 <gre20018@byui.edu>
2026-09-21 16:38:34 -0600

    Got everything set up FRFR this time

---
+++ server.js
@@ -0,0 +1,37 @@
+// Imports
+import express from "express";
+import { fileURLToPath } from "url";
+import path from "path";
+
+/**
+ * Declare Important Variables
+ */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const PORT = process.env.PORT || 3000;
+
+/**
+ * Setup Express Server
+ */
+const app = express();
+app.use(express.static(path.join(__dirname, "public")));
+
+/**
+ * Declare Routes
+ */
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "src/views/home.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "src/views/about.html"));
+});
+
+app.get("/products", (req, res) => {
+  res.sendFile(path.join(__dirname, "src/views/products.html"));
+});
+
+// Start the server and listen on the specified port
+app.listen(PORT, () => {
+  console.log(`Server is running on http://127.0.0.1:${PORT}`);
+});
