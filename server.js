4eb7504eb1ebb9d43aff8d7b1c520a00ee22cb8c
ksg721 <gre20018@byui.edu>
2026-09-23 22:37:46 -0600

    Switched from html to ejs

--- server.js
+++ server.js
@@ -9,26 +9,44 @@ import path from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const PORT = process.env.PORT || 3000;
+const NODE_ENV = process.env.NODE_ENV || 'production';
 
 /**
  * Setup Express Server
  */
 const app = express();
+
+/**
+ * express middleware
+ */
 app.use(express.static(path.join(__dirname, "public")));
+
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+
+// Tell Express where to find your templates
+app.set('views', path.join(__dirname, 'src/views'));
 
 /**
  * Declare Routes
  */
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "src/views/home.html"));
+
+/**
+ * Routes
+ */
+app.get('/', (req, res) => {
+    const title = 'Welcome Home';
+    res.render('home', { title });
 });
 
-app.get("/about", (req, res) => {
-  res.sendFile(path.join(__dirname, "src/views/about.html"));
+app.get('/about', (req, res) => {
+    const title = 'About Me';
+    res.render('about', { title });
 });
 
-app.get("/products", (req, res) => {
-  res.sendFile(path.join(__dirname, "src/views/products.html"));
+app.get('/products', (req, res) => {
+    const title = 'Our Products';
+    res.render('products', { title });
 });
 
 // Start the server and listen on the specified port
